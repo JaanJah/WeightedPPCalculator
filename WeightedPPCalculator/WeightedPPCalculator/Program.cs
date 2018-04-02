fd151e94be7db7d18d773d917ede76ee685d51d3
jaanjahimees <jaanmarkus.jahilo@tptlive.ee>
2018-04-02 22:10:00 +0300

    done

---
+++ WeightedPPCalculator/WeightedPPCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeightedPPCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] ppScores = new double[100];
+            List<double> ppList = new List<double>();
+            double totalPP = 0;
+            for (int i = 0; i < ppScores.Length; i++)
+            {
+                Console.WriteLine("Enter your {0}. score", i + 1);
+                double score = double.Parse(Console.ReadLine());
+                ppScores[i] = score;
+                ppList.Add(ppScores[i] * Math.Pow(0.95, (i)));
+            }
+            //For testing purposes
+            foreach (double item in ppList)
+            {
+                Console.WriteLine("Weighted pp: {0}", item);
+                totalPP += item;
+            }
+            Console.WriteLine(totalPP);
+            Console.ReadLine();
+        }
+    }
+}
